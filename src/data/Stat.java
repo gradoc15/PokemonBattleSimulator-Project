88da81e8fbad77efd0862cb0553bbb6e305b5ed8
Hang AmI <gradoc15@new.edvhtl.at>
2019-05-13 10:07:07 +0200

    added stat enums

---
+++ src/data/Stat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author User
+ */
+public enum Stat
+{
+    HP,
+    ATK,
+    DEF,
+    SPATK,
+    SPDEF,
+    INIT;
+}
