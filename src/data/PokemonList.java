ffa9fc91e4a2cadbd079eb0ee59bebd0a887973e
Hang AmI <gradoc15@new.edvhtl.at>
2019-05-13 10:22:40 +0200

    added enum, where valid pokemons can be listed

--- src/data/PokemonList.java
+++ src/data/PokemonList.java
@@ -9,23 +9,15 @@ package data;
  *
  * @author User
  */
-public class Pokemon
+public enum PokemonList
 {
-    //
+    A();
+    
+    private PokemonList()
+    {
+        
+    }
     private int id;
     private String name;
-    private Ability ability;
-    private Nature nature;
-    private Gender gender;
-    private int lvl;
-    private Move[] move = new Move[4];
-    private boolean shiny;
-    
     private Values basicValues;
-    private Values iv;
-    private Values ev;
-    
-    private int currentHP;
-   
-   
 }
