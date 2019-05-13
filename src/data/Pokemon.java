7d4941a177fc123fa9355a26e576d399bdf50138
Hang AmI <gradoc15@new.edvhtl.at>
2019-05-13 10:51:29 +0200

    stats are now calculated by the basicvalues, ivs and evs

--- src/data/Pokemon.java
+++ src/data/Pokemon.java
@@ -11,9 +11,22 @@ package data;
  */
 public class Pokemon
 {
-    //
-    private int id;
-    private String name;
+    public Pokemon(PokemonList pl)
+    {
+        id = pl.getId();
+        name = pl.getName();
+        basicValues = pl.getBasicValues();
+        
+        iv = new Values(31, 31, 31, 31, 31, 31);
+        ev = new Values(0, 0, 0, 0, 0, 0);
+        lvl = 100;
+        ability = pl.getPossibleAbilities()[0];
+    }
+    
+    private final int id;
+    private final String name;
+    private final Values basicValues;
+    
     private Ability ability;
     private Nature nature;
     private Gender gender;
@@ -21,7 +34,6 @@ public class Pokemon
     private Move[] move = new Move[4];
     private boolean shiny;
     
-    private Values basicValues;
     private Values iv;
     private Values ev;
     
